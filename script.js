e5981b1a066ea379b0e4d77a817c753dfdfb2a32
zfjrblxx <zfjrblxx@gmail.com>
2026-07-26 15:50:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 
 console.log("Portfolio loaded.");
 
+// ==============================
+// AUTO AGE
+// ==============================
+
+// Ganti 2000 dengan tahun lahir kamu
+const birthDate = new Date(2000, 6, 28);
+// Bulan dimulai dari 0
+// 6 = Juli
+
+function calculateAge() {
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const birthdayThisYear = new Date(
+        today.getFullYear(),
+        birthDate.getMonth(),
+        birthDate.getDate()
+    );
+
+    if (today < birthdayThisYear) {
+        age--;
+    }
+
+    document.getElementById("age").textContent = age;
+}
+
+calculateAge();
+
